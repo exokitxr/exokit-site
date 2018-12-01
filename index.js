b12f8c000e9dce063d95f6f7e533505804e1b63c
Avaer Kazmer <a@modules.io>
2018-11-30 19:16:39 -0500

    Add initial hero design

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const express = require('express');
 
+const port = process.env['PORT'] || 8000;
+
 const app = express();
 app.get('*', express.static(__dirname));
 http.createServer(app)
-  .listen(8000);
+  .listen(port);

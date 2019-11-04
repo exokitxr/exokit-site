dffa3b53bac58b233778c0bce123e331e129acf9
Avaer Kazmer <a@modules.io>
2019-11-04 14:25:59 -0500

    Rewrite start script

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
+#!/bin/bash
+
 const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser')
-const nodemailer = require('nodemailer');
-
-const port = process.env['PORT'] || 8000;
 
 const app = express();
-app.get('*', express.static(__dirname));
-
-app.post('/sendEmail', bodyParser.json(), (req, res) =>{
-  console.log(req.body);
-  res.send("hello");
-
-  if(req.body.fName != '' && req.body.lName != '' &&
-   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email) === true && req.body.message != '')
-   {
-    const transporter = nodemailer.createTransport('smtps://exokitsender@gmail.com:ExoEmail2!@smtp.gmail.com');
-
-    const mailOptions = {
-      from: `"${req.body.fName} ${req.body.lName}, ${req.body.email}`,
-      to: 'Exokit, hello@webmr.io',
-      subject: 'New inquiry from exokit.org',
-      text: req.body.message + "\n\n" +
-      "Company: " + req.body.company + "\n\n" +
-      "Name: " + req.body.fName + " " + req.body.lName + "\n\n" +
-      "Sender Email: " + req.body.email
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            return console.log(error);
-        }
-        console.log('Message sent: ' + info.response);
-    });
-  }
-  else{
-    console.log('email info does not meet needs')
-  }
-
-
-});
+app.use(express.static(__dirname));
 
 http.createServer(app)
-  .listen(port);
+  .listen(80)
+
+console.log(`http://dev.exokit.org`);

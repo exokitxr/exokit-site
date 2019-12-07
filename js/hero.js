518d49e9105bd1a68a5470ccaa0a799f8fb745e0
Avaer Kazmer <a@modules.io>
2019-12-06 19:25:31 -0500

    Animate miku model

--- js/hero.js
+++ js/hero.js
@@ -800,6 +800,27 @@ function animate() {
     .add(localVector.set((mouse.x - 0.5)*2*2, -(mouse.y - 0.5)*2*2, 0));
   exobotMesh.rotation.z = Math.sin(factor * Math.PI*2/2)*0.2;
 
+  rig.inputs.hmd.position.set(0, 1.3, 0);
+  rig.inputs.leftGamepad.position.set(0.3, 0.7, 0.1);
+  rig.inputs.leftGamepad.quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI/2*0.7);
+  rig.inputs.leftGamepad.pointer = 1;
+  rig.inputs.leftGamepad.grip = 1;
+  rig.inputs.rightGamepad.position.set(-0.2, 1, 0);
+  rig.inputs.rightGamepad.position.add(
+    new THREE.Vector3(0, 0, -0.3)
+      .applyQuaternion(new THREE.Quaternion().setFromUnitVectors(
+        new THREE.Vector3(0, 0, -1),
+        exobotMesh.position.clone().sub(rig.inputs.rightGamepad.position).normalize()
+      ))
+  );
+  rig.inputs.rightGamepad.quaternion.setFromUnitVectors(
+    new THREE.Vector3(0, 0, -1),
+    exobotMesh.position.clone().sub(rig.inputs.rightGamepad.position).normalize()
+  ).multiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI/2*0.3));
+  rig.inputs.rightGamepad.pointer = 0;
+  rig.inputs.rightGamepad.grip = 1;
+  rig.update();
+
   if (now > meteorMesher.nextUpdateTime) {
     const meteorMesh = _makeMeteorMesh();
     meteorMesher.add(meteorMesh);
@@ -824,7 +845,6 @@ function animate() {
   renderer.render(scene, camera);
   lastUpdateTime = now;
 }
-
 renderer.setAnimationLoop(animate);
 
 /* window.addEventListener("scroll", e =>{
